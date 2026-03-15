38ad4238fc721fff91b6c79ea752dc90ea8abe60
Rohini1204 <rohinitirupati16@gmail.com>
2026-03-15 19:55:32 +0530

    Change app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def predict_alzheimers():
     return jsonify({"result": int(pred)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
